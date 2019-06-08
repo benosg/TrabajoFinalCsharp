b3b5d62debf6bfa00e16e6d05956d0452b9a1e9e
benosg <benosg@gmail.com>
2019-06-08 11:51:55 -0400

    Agregar archivos de proyecto.

---
+++ entity/Factura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entity
+{
+   public class Factura
+    {
+        public int numFactura { get; set; }
+        public string  rutCliente { get; set; }
+        public string razonSocial { get; set; }
+        public DateTime fecha { get; set; }
+        public int iva { get; set; }
+        public int neto { get; set; }
+        public int total { get; set; }
+
+        public int codProducto { get; set; }
+        public int cantidad { get; set; }
+        public int subTotal { get; set; }
+
+        public List<Factura> lstFactura { get; set; }
+
+    }
+}
---
+++ entity/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entity
+{
+    public class Usuario
+    {
+        public string rut { get; set; }
+        public string  nombre { get; set; }
+        public int sistema { get; set; }
+        public List<Usuario> lstUsuario{ get; set; }
+}
+}
